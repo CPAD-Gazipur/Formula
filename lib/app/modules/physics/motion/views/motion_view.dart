271c022a7cf4f840ccdf3f34f50e6f5e95b55e38
Your Name <you@example.com>
2022-01-14 23:51:01 +0600

    Motion page edited

--- lib/app/modules/physics/motion/views/motion_view.dart
+++ lib/app/modules/physics/motion/views/motion_view.dart
@@ -9,13 +9,46 @@ class MotionView extends GetView<MotionController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Motion'),
+        title: Text('Motion(গতি)'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'MotionView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "[u=আদি বেগ,\nv=শেষ বেগ,\ns/h=অতিক্রান্ত দুরত্ব/উচ্চতা,\nt=সময়,\na=ত্বরণ,\ng=অভিকর্ষজ ত্বরণ]",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '1.v = u + at',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '2.v² = u² + 2as',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "3.s = ut + ½at²",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "4.s = ½(u+v)*t",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "5.v² = u² + 2gh",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "6.h = ut + ½gt²",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "7.h = ½(u+v)*t",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
         ),
       ),
     );
