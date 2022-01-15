361485d12c480b35a49ac12a98069e2d549b3c1b
Your Name <you@example.com>
2022-01-15 10:44:04 +0600

    Wave page contributed

--- lib/app/modules/physics/wave/views/wave_view.dart
+++ lib/app/modules/physics/wave/views/wave_view.dart
@@ -9,13 +9,46 @@ class WaveView extends GetView<WaveController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wave'),
+        title: Text('WaveView'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'WaveView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "[V/v=বেগ,\nT=তাপমাত্রা,\ns=আপেক্ষিক সুপ্ত তাপ,\n𝛌=তরঙ্গ দৈর্ঘ্য,\nf=কম্পাঙ্ক,\nT=পর্যায়কাল,\nd=অতিক্রান্ত দুরত্ত,\nN=কম্পন সংখা,\nv`=বাতাসে শব্দের বেগ]",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '1.d=½(v*t)',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '2.d=vt',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "3.½mv²=ms∆T",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "4.T=1/f",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "5.s=N𝛌",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "6.v=(𝛌/T)=f𝛌",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "7.V=v`+(0.6*T)",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
         ),
       ),
     );
